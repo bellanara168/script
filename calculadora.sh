33f80541e7e4ddd900376b17b18cd032ae9d2809
Isabella Nara Costa Alves <isabellanarac@gmail.com>
2025-02-18 14:21:34 -0300

    Add files via upload

---
+++ calculadora.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define os números
+num1=2
+num2=2
+
+# Calcula a soma
+resultado=$(expr $num1 + $num2)
+
+# Imprime o resultado
+echo "O resultado de $num1 + $num2 é: $resultado"
+
+
+
